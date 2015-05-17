4dd1d2b76298ab6643dbd08f3c6fd9e6ceae2daa
Simon Krisch <Simon.Krisch@gmx.de>
2015-05-17 19:30:20 +0200

    added temporary menu

--- main.cpp
+++ main.cpp
@@ -9,23 +9,32 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	Baum b;
-	b.insert(5);
+	/*b.insert(5);
 	for(int i=0;i<10;i++)
 		if(i==5) continue;
 		else b.insert(i);
 
 
-	Writer w(b);
+	*/Writer w(b);/*
 	w.dump();
 
 	b.remove(5);
 	w.dump();
 	b.remove(1);
 	cout << "ok" << endl;
-	w.dump();
+	w.dump();*/
+	while(true) {
+		int d=0;
+		cout << "insert [-1:=abort] ?= ";
+		cin >> d;
+		if(d==-1)break;
+		b.insert(d);
+		w.dump();
+	}
 	ofstream ofdot("tree.dot");
 	w.writeDot(ofdot);
 	system("dot -Tpng tree.dot -o tree.png");
+	cout << "image written (tree.png)" << endl;
 	//system("ristretto tree.png 2>/dev/null");
 
 	return 0;
